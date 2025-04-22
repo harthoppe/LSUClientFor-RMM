04dfe1c7fa407d687023b39a3f3dce539c3d93e2
hhoppework <hhoppe@ckr.com>
2025-04-22 15:20:35 -0500

    Remove unnecessary Start-Transcript and Stop-Transcript calls in Update-Lenovo script for cleaner execution flow

--- Update-Lenovo.ps1
+++ Update-Lenovo.ps1
@@ -1,7 +1,5 @@
 # utilizes LSUClient with adjustments for NinjaOne RMM interactivity
 
-Start-Transcript -Path "$($env:TEMP)\Update-Lenovo.txt" -Append
-
 try {
     $batteryStatus = Get-WmiObject -Class BatteryStatus -Namespace root\wmi
     if ($batteryStatus.PowerOnLine -eq $false) {
@@ -26,26 +24,20 @@ if ($env:maxExtractRuntime) {
     Set-LSUClientConfiguration -MaxExtractRuntime (New-TimeSpan -Minutes 20) -Verbose
 }
 
-# Set a maximum allowed installer runtime of 20 minutes if set by NinjaOne
-$MaxRounds = $($env:maxRounds)
-
-for ($Round = 1; $Round -le $MaxRounds; $Round++) {
+for ($Round = 1; $Round -le $($env:maxRounds); $Round++) {
     Write-Host "Starting round $Round"
     $updates = Get-LSUpdate -FailUnsupportedDependencies -Verbose
     Write-Host "$($updates.Count) updates found"
     if ($updates.Count -eq 0) {
         break;
     }
-    $updates | Save-LSUpdate  -Path $env:TEMP -Verbose -ShowProgress
+    $updates | Save-LSUpdate  -Path $env:TEMP -Verbose
     [array]$results = Install-LSUpdate -Path $env:TEMP -Package $updates -Verbose
 }
 
 # Either restart the computer or display a remediation message
 if ($env:forceRestartComputer -eq $true) {
-    Stop-Transcript
     Restart-Computer
 }  else {
     msg * "Critical updates have been installed. Please restart your computer immedietly to complete the installation."
 }
-
-Stop-Transcript
